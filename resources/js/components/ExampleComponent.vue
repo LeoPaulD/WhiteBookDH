3f5a7e06c09409f069543fb89b01ca934a01ff1a
leopauld <leopauldubourg@gmail.com>
2021-11-18 12:27:17 +0100

    maj page map

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -1,49 +1,67 @@
 <template>
 
-  <div>
+  <div >
     
+    <div  v-if="info" style="position: absolute; height: 90%; width: 30vw; z-index: 1000;" class="bg-light">
+      <info-component></info-component>
+    </div>
     <l-map
       v-if="showMap"
       :zoom="zoom"
       :center="center"
       :options="mapOptions"
-      style="height: 100%; width: 100%"
+      
+      style="height: 90%; width: 100vw;"
       @update:center="centerUpdate"
       @update:zoom="zoomUpdate"
+      
     >
       <l-tile-layer
         :url="url"
         :attribution="attribution"
       />
-      <l-marker :lat-lng="withPopup">
-        <l-popup>
-          <div @click="innerClick">
-            I am a popup
+      <l-control-zoom position="topright"  ></l-control-zoom>
+      
+      
+
+      <l-marker :lat-lng="withPopup" v-on:click="show">
+        
+         <l-icon
+          :icon-size="dynamicSize"
+          
+          
+        >
+          
+          <img src="/img/illu/school.svg">
+          <div class=" text-center">
             
           </div>
-        </l-popup>
+        </l-icon>
+        
+
+        <l-tooltip> 
+         
+          
+          <div >
+            CUNY Graduate Center
+            
+          </div>
+        </l-tooltip>
+        
       </l-marker>
       
     </l-map>
-    <!-- <div>
-      <p>First marker is placed at {{ withPopup.lat }}, {{ withPopup.lng }}</p>
-      <p>Center is at {{ currentCenter }} and the zoom is: {{ currentZoom }}</p>
-      
-    </div> -->
+    
   </div>
   
 </template>
 
 <script>
+
+
 import { latLng } from 'leaflet';
-import {
-  LMap,
-  LTileLayer,
-  LMarker,
-  LPopup,
-  
-  
-} from 'vue2-leaflet';
+import { LMap, LTileLayer, LControl,LMarker,LCircleMarker,LTooltip,LFeatureGroup, LPopup, LControlLayers, LControlZoom, LIcon, LWMSTileLayer} from "vue2-leaflet";
+import InfoComponent from './InfoComponent.vue';
 
 export default {
   name: 'Example',
@@ -52,27 +70,48 @@ export default {
     LTileLayer,
     LMarker,
     LPopup,
+    LCircleMarker,
+    LControl,
+    LFeatureGroup,
+    LTooltip,
+    LControlLayers,
+    LControlZoom,
+    LIcon,
+    InfoComponent
     
   },
   data() {
     return {
+      info: false, 
+      
+      iconSize: 40,
+      
       zoom: 13,
-      center: latLng(47.41322, -1.219482),
+      center: latLng(40.74881093824601, -73.98398554515835),
       url: 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
       attribution:
         '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
-      withPopup: latLng(47.41322, -1.219482),
-      withTooltip: latLng(47.41422, -1.250482),
+      withPopup: latLng(40.74881093824601, -73.98398554515835),
+      
       currentZoom: 11.5,
-      currentCenter: latLng(47.41322, -1.219482),
+      currentCenter: latLng(440.74881093824601, -73.98398554515835),
       showParagraph: false,
       mapOptions: {
         zoomSnap: 0.5,
+        zoomControl: false
       },
       showMap: true,
     };
+
+  },
+  computed: {
+    dynamicSize() {
+      return [this.iconSize /1.8, this.iconSize ];
+    },
+    
   },
   methods: {
+    
     zoomUpdate(zoom) {
       this.currentZoom = zoom;
     },
@@ -82,9 +121,14 @@ export default {
     showLongText() {
       this.showParagraph = !this.showParagraph;
     },
-    innerClick() {
-      alert('Click!');
-    },
+    show: function () {
+        if (this.info == false) {
+          this.info = true;
+        } else {
+          this.info = false;
+        }
+
+      },
   },
 };
 </script>
---
+++ resources/js/components/InfoComponent.vue
@@ -0,0 +1,41 @@
+<template>
+
+  <div >
+    
+      
+      <div>
+        <h1 class="baskerville text-center m-2"> <b>Master in Digital Humanities</b></h1>
+        <p class="noto"> The M.A. in Digital Humanities program cultivates both the practical skills and the humanistic knowledge needed to work on digital humanities-related projects in universities, libraries, museums, and non-profit cultural organizations.</p>
+      </div>
+    
+    
+         
+  </div>
+  
+</template>
+
+<script>
+
+
+
+export default {
+  name: 'Example',
+  components: {
+   
+  },
+  data() {
+    return {
+      
+    };
+
+  },
+  computed: {
+    
+    
+  },
+  methods: {
+    
+    
+  },
+};
+</script>
