e9d7ca7a5d6a0ccf7acd3d28d18ae2c3c04fc8b1
leopauld <leopauldubourg@gmail.com>
2021-12-12 23:36:48 +0100

    maj ajout sphère guillaume

--- resources/views/opensphere/index.blade.php
+++ resources/views/opensphere/index.blade.php
@@ -5,7 +5,7 @@
 @section('content')
 <div style="height: 70px;"></div>
 <div >
-    <iframe style="height: 91vh; width: 100vw;" src="https://hyperotlet.huma-num.fr/otletosphere/" frameborder="0"></iframe>
+    <iframe style="height: 91vh; width: 100vw;" src="https://dev.myllaume.fr/dhsphere/" frameborder="0"></iframe>
 </div>
 
 @endsection
